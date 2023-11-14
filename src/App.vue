0ccec405a5eac49775be168f084cedd2f9096d0f
HUNGNV10-D5\HungNV10 <hung.nv155790hust@gmail.com>
2023-11-14 17:32:06 +0700

    feat(common): create prompt component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="$route?.meta?.layout || 'div'"></component>
+</template>
+
+<script>
+import { useI18n } from "vue-i18n";
+
+export default {
+  setup() {
+    const { t, locale } = useI18n();
+    return {
+      t,
+      locale,
+    };
+  },
+};
+</script>
